70ab6d6456d10f2c7078a4229cac059669b3c2e9
kammarimahesh123 <117282075+kammarimahesh123@users.noreply.github.com>
2022-11-16 12:06:49 +0530

    Update p14.js

--- promises/p14.js
+++ promises/p14.js
@@ -16,22 +16,8 @@ function findlocation(response){
         resolve("Extra information" +response)
     })
 };
-// const newLocal = location("google").then(response => {
-//     console.log("response received");
-//     return findlocation(response);
-// }).then(responsereceived => { console.log(responsereceived); });
+ const newLocal = location("google").then(response => {
+     console.log("response received");
+     return findlocation(response);
+ }).then(responsereceived => { console.log(responsereceived); });
 
-async function getlocation(){
-    try{
-    var res=await location("facebook");
-    //console.log("response received")
-    var res1=await findlocation(res)
-    console.log(res1)
-}catch(err){
-    console.log(err)
-}finally{
-    console.log("I try to find the location")
-}
-
-}
-getlocation();
