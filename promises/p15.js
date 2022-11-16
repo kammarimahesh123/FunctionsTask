4f9751e0910b44b146030d1a9288736845dac6bb
Kammarimahesh123 <kammarimahesh1@123>
2022-11-16 12:03:08 +0530

    p15 added

---
+++ promises/p15.js
@@ -0,0 +1,32 @@
+
+function location(location){
+    console.log("Do something to find the location")
+    return new Promise((resolve,reject)=>{
+        if(location==="google")
+        resolve("Google says --HI" )
+        else
+        reject("Couldn't find the location")       
+    f});
+
+};
+
+function findlocation(response){
+    console.log("Found the location");
+    return new Promise((resolve,reject)=>{
+        resolve("Extra information" +response)
+    })
+};
+async function getlocation(){
+    try{
+    var res=await location("facebook");
+    //console.log("response received")
+    var res1=await findlocation(res)
+    console.log(res1)
+}catch(err){
+    console.log(err)
+}finally{
+    console.log("I try to find the location")
+}
+
+}
+getlocation();
