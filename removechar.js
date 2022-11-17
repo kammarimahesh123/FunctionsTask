160a7dd05d04a52e9d0880e7b3df9226176af2d7
Kammarimahesh123 <kammarimahesh1@123>
2022-11-17 15:31:31 +0530

    modified

--- removechar.js
+++ removechar.js
@@ -1,5 +1,34 @@
+let str4="maheshkammari"
+
+let sub=""
+for(var i=2; i<=str4.length-3;i++)
+{
+
+    sub=sub+str4[i]
+  
+}
+console.log(sub)
+///////////////////////////////////////////////////
 let str="kammariMahesh"
 
 let remove=str.slice(1,10)
 
 console.log(remove)
+
+///////////////////////////////////////
+
+let str1="I love my India"
+
+let str2=str1.split(" ")
+
+console.log(str2[2])
+//////////////////////////////////////////////
+
+let string="mahesh";
+
+let result=string.substring(1,4)
+
+console.log(result)
+////////////////////////////////////////////////////////////
+
+
