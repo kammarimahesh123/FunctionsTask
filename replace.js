6fcde80f021dc4dcd2c916470abc422f2968578e
Kammarimahesh123 <kammarimahesh1@123>
2022-11-17 10:54:44 +0530

    updated

--- replace.js
+++ replace.js
@@ -3,3 +3,21 @@ let nums=[2,3,4,5,6,7,8]
 let numsreplace=nums.fill(0)
 
 console.log(numsreplace)
+
+////////////////////////////////////
+
+
+ 
+function replace(n)
+{
+    let nums1=[2,3,4,5,6,7,8]
+    for(i=0;i<=nums1.length;i++)
+    {
+       nums1[i]=n;
+       //return nums1[i];
+    }
+    return nums1[i];
+
+}
+console.log(replace(9));
+//////////////////////////////////
