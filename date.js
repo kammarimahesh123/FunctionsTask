e8e0bfc25bb0fc16797a1e465414b62841e6979e
Kammarimahesh123 <kammarimahesh1@123>
2022-11-17 10:32:31 +0530

    updated

--- date.js
+++ date.js
@@ -1,1 +1,17 @@
 console.log(Date())
+////////////////
+
+let date=new Date();
+
+console.log("Date:" ,date)
+
+
+////////////////////////////////
+
+let d=new Date().getDay();
+
+let m=new Date().getMonth();
+
+let y=new Date().getFullYear();
+
+console.log(`Day :${d} MOnth: ${m} Year: ${y}`)
